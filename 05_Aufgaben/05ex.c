bdf35e53e841d9fed50d722e3f25f44ddbfcbdbb
ZionDere2 <165028736+ZionDere2@users.noreply.github.com>
2025-11-05 13:40:57 +0100

    Add files via upload

---
+++ 05_Aufgaben/05ex.c
@@ -0,0 +1,60 @@
+/*
+Willkommen zum fünften Aufgabenblatt vom Programmierkurs. Auf diesem Aufabenblatt geht es um Rekursion.
+
+Um die Tests für dieses Blatt zu kompilieren und zu starten, führen Sie den folgenden Befehl aus:
+clang -std=c11 -g -Wall 05ex_test.c -o 05ex_test.o -lm && ./05ex_test.o
+*/
+
+#include "05_canvas.h"
+#include <stdio.h>
+#include <math.h>
+
+/*
+Aufgabe 1a:
+Zeichnen Sie eine horizontale Linie der Länge `width`, deren am weitesten links liegender Pixel bei `(x, y)` ist.
+Wenn Teile der Linie außerhalb der Canvas liegen, dann sollen diese Teile ignoriert werden. Alles was in die Canvas passt, muss aber gezeichnet werden.
+
+_Benutzen Sie keine Schleifen - Die Aufgabe soll über Rekursion gelöst werden!_
+
+*/
+Canvas recursive_line(Canvas c, int x, int y, int width) {
+    return c;
+}
+
+/*
+Aufgabe 1b:
+Zeichnen Sie ein Rechteck mit der Breite `width` und der Höhe `height`. Der Pixel der linken unteren Ecke liegt bei `(x, y)`.
+Wenn Teile des Rechtecks außerhalb der Canvas liegen, dann sollen diese Teile ignoriert werden. Alles was in die Canvas passt, muss aber gezeichnet werden.
+
+_Benutzen Sie keine Schleifen, die Aufgabe soll über Rekursion gelöst werden!_
+*/
+Canvas recursive_rectangle(Canvas c, int x, int y, int width, int height) {
+    return c;
+}
+
+/*
+Aufgabe 2:
+Die Fibonaccizahlen sind wie folgt definiert:
+
+- die nullte Fibonaccizahl ist 1,
+- die erste Fibonaccizahl ist ebenfalls 1, und
+- jede weitere Fibonaccizahl ist die Summe der zwei vorigen Fibonaccizahlen.
+
+Berechne die `n`-te Fibonaccizahl.
+*/
+int fibonacci(int n) {
+    return 0;
+}
+
+/*
+Aufgabe 3:
+Implementieren Sie einen schwarzen Fülleimer. Gegeben eine Koordinate `(x, y)` auf einer (bereits bemalten) Canvas, soll die komplette
+zusammenhängende Fläche aller Pixel der selben Farbe (schwarz oder weiß) schwarz gefärbt werden.
+Zwei Pixel sind Teil der selben Fläche wenn sie die selbe Farbe haben und direkt benachbart sind. Jeder Pixel hat bis
+zu vier direkte Nachbarn - die Diagonalen zählen nicht.
+
+Funktionen, um die Farbe eines Pixels auf der Canvas zu bestimmen, sind im Headerfile der Canvas dokumentiert.
+*/
+Canvas bucket_fill(Canvas c, int x, int y) {
+    return c;
+}
